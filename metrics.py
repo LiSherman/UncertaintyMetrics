0eda4826a1a2f7d04628173104dfd590591a4a38
Shiman Li <96482156+LiSherman@users.noreply.github.com>
2024-03-15 13:44:26 +0800

    Add files via upload

--- metrics.py
+++ metrics.py
@@ -1,22 +1,24 @@
 import numpy as np
 from sklearn.neighbors import KernelDensity
-def uncertainty_metrics(uncertainty,prediction,groundTruth):
+import matplotlib.pyplot as plt
+
+def cal_UCM(uncertainty,prediction,groundTruth):
     union = np.logical_or(prediction,groundTruth)
     correct = np.logical_and(np.logical_and(prediction,groundTruth),union)
     error = np.logical_and(np.logical_xor(prediction,groundTruth),union)
-    cor_distr = KernelDensity().fit(uncertainty(np.where(correct)))
-    bins = 1000
-    x = np.linspace(0,1,bins)
-    cor_distribution = cor_distr.score_samples(x)
-    err_distr = KernelDensity().fit(uncertainty(np.where(error)))
-    err_distribution = err_distr.score_samples(x)
-    idx, _ = np.where((err_distribution>=cor_distribution)==True)
+    cor_distr = KernelDensity(bandwidth=0.01).fit(uncertainty[np.where(correct)][:, np.newaxis])
+    bins = 200
+    x = np.linspace(0,1,bins)[:, np.newaxis]
+    cor_distribution = np.exp(cor_distr.score_samples(x))
+    err_distr = KernelDensity(bandwidth=0.01).fit(uncertainty[np.where(error)][:, np.newaxis])
+    err_distribution = np.exp(err_distr.score_samples(x))
+    idx = np.where((err_distribution>=cor_distribution)==True)[0]
     inser_idx = idx[0]
     UCM = (sum(err_distribution[:inser_idx])+sum(cor_distribution[inser_idx:]))/(sum(cor_distribution)+sum(err_distribution))
     threshold = inser_idx/bins
     return UCM
 
-def ESCE(uncertainty,prediction,groundTruth,threshold):
+def cal_ESCE(uncertainty,prediction,groundTruth,threshold):
     union = np.logical_or(prediction,groundTruth)
     correct = np.logical_and(np.logical_and(prediction,groundTruth),union)
     low_uncertainty = np.logical_and(uncertainty<threshold,union)
@@ -25,7 +27,7 @@ def ESCE(uncertainty,prediction,groundTruth,threshold):
     ESCE = np.abs(u_dice-dice)
     return ESCE
 
-def HDice(uncertainty,prediction,groundTruth,threshold):
+def cal_HDice(uncertainty,prediction,groundTruth,threshold):
     union = np.logical_or(prediction,groundTruth)
     correct = np.logical_and(np.logical_and(prediction,groundTruth),union)
     low_uncertainty = np.logical_and(uncertainty<threshold,union)
@@ -35,10 +37,3 @@ def HDice(uncertainty,prediction,groundTruth,threshold):
     reliable_dice = reliable_correct.sum() * 2/(low_uncertainty.sum()+np.logical_xor(correct,high_uncertainty).sum())
     Hdice = 2/(1./reliable_dice+1./reliable_ratio)
     return Hdice
-
-
-
-
-
-
-    
